a32317badb50708d9d4bac9941240bb6b3bfd527
Leloo <sabrina.stefa@gmail.com>
2022-03-22 13:14:51 +0100

    add (page)

---
+++ views/page/introStartGame.php
@@ -0,0 +1,10 @@
+<div class="container-fluid bg">
+	<div class="row ">
+		<div>
+			<img class="gif justify-content-center" src="public/images/animStart2.gif" alt="">
+		</div>
+	</div>
+	<div class="btnStart">
+		<a href="?section=startGame"><img class="btnStartImg" src="public/images/btnStart.png" alt=""></a>
+	</div>
+</div>
